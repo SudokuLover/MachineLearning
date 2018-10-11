d864bf35d988981b085a5bd1450717a8f50d66e6
Gaurang <Gauranggoel.9@gmail.com>
2018-10-11 13:27:32 +0530

    Sampling

--- Part 6 - Reinforcement Learning/Section 33 - Thompson Sampling/thompson_sampling1.py
+++ Part 6 - Reinforcement Learning/Section 33 - Thompson Sampling/thompson_sampling1.py
@@ -1,4 +1,4 @@
-#Upper Confidence Band
+#Thomson Sampling
 
 #import the libraries
 import pandas as pd
@@ -8,34 +8,29 @@ import matplotlib.pyplot as plt
 #importing dataset
 dataset = pd.read_csv('ads_CTR_Optimisation.csv')
 
-#implementing UCB
-import math
+#implementing Thomson Sampling
+import random
 N=10000
 d=10
 ads_selected=[]
-number_of_selections=[0]*d
-sum_of_rewards=[0]*d
+number_of_rewards_1=[0]*d
+number_of_rewards_0=[0]*d
 total_rewards=0
-k=0
+
 for n in range(0,N):
     ad=0
-    max_upper_bound=0
+    max_random=0
     for i in range(0,d):
-        if(number_of_selections[i]>0):
-            average_rewards=sum_of_rewards[i]/number_of_selections[i]
-            delta_i=math.sqrt(3/2*math.log(n+1)/number_of_selections[i])
-            upper_bound=average_rewards+delta_i
-            
-        else :
-            upper_bound=1e400
-        
-        if(upper_bound>max_upper_bound):
-            max_upper_bound=upper_bound
+        random_beta=random.betavariate(number_of_rewards_1[i]+1,number_of_rewards_0[i]+1)
+        if(random_beta>max_random):
+            max_random=random_beta
             ad=i
     ads_selected.append(ad)
-    number_of_selections[ad]=number_of_selections[ad]+1
     rewards=dataset.values[n,ad]
-    sum_of_rewards[ad]=sum_of_rewards[ad]+rewards
+    if rewards==1:
+        number_of_rewards_1[ad]=number_of_rewards_1[ad]+1
+    else:
+        number_of_rewards_0[ad]=number_of_rewards_0[ad]+1
     total_rewards=total_rewards + rewards
 
         
