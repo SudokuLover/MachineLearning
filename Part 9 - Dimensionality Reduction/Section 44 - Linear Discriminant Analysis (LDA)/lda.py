2115ef2050c1d4665f1cba6a4e6cebbdcbcd9ea0
Gaurang <Gauranggoel.9@gmail.com>
2018-10-19 01:07:06 +0530

    pca

--- Part 9 - Dimensionality Reduction/Section 44 - Linear Discriminant Analysis (LDA)/lda.py
+++ Part 9 - Dimensionality Reduction/Section 44 - Linear Discriminant Analysis (LDA)/lda.py
@@ -32,6 +32,7 @@ from sklearn.linear_model import LogisticRegression
 classifier = LogisticRegression(random_state = 0)
 classifier.fit(X_train, y_train)
 
+
 # Predicting the Test set results
 y_pred = classifier.predict(X_test)
 
